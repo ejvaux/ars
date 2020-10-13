42bd39adbeff9467f73c9bc31ec27ceb06063248
Edmund Mati <P000199@SERCOMM.COM>
2020-10-13 17:22:11 +0800

    Migration - create/add col. create emp_type, employee add type

--- database/migrations/2020_10_13_082708_create_emp_types_table.php
+++ database/migrations/2020_10_13_082708_create_emp_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEmployeesTable extends Migration
+class CreateEmpTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateEmployeesTable extends Migration
      */
     public function up()
     {
-        Schema::create('employees', function (Blueprint $table) {
+        Schema::create('emp_types', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->integer('type_id');
-            $table->integer('department_id');
-            $table->integer('site_id');
-            $table->date('birthdate');
+            $table->string('name');            
+            $table->string('description')->nullable();            
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateEmployeesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('emp_types');
     }
 }
