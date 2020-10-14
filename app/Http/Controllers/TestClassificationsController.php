3509aca572d525b0b3b9c5594d70ac1d8e7cb03a
Edmund Mati <P000199@SERCOMM.COM>
2020-10-14 11:37:26 +0800

    changes

---
+++ app/Http/Controllers/TestClassificationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\test_classifications;
+use Illuminate\Http\Request;
+
+class TestClassificationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\test_classifications  $test_classifications
+     * @return \Illuminate\Http\Response
+     */
+    public function show(test_classifications $test_classifications)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\test_classifications  $test_classifications
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(test_classifications $test_classifications)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\test_classifications  $test_classifications
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, test_classifications $test_classifications)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\test_classifications  $test_classifications
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(test_classifications $test_classifications)
+    {
+        //
+    }
+}
