8dd3a4a85f50ad2e5106bd2ed3311989151f385f
Lawrence Bondad <P000269@SERCOMM.COM>
2020-10-16 17:03:52 +0800

    101%

--- public/js/resultrecord.js
+++ public/js/resultrecord.js
@@ -18,23 +18,9 @@ $("#resultRecSubmitBtn").on("click", function(e) {
                 alert(data)
 
             },
-            // error:function(error){
-            //     alert("Cant find the Employee ID");
-            // }
+            
         });
     }
     
 });
 
-
-// else if($('#result_id').val().length == 0){
-    //     alert('Please enter result on form')
-    // }  
-
-// $("#resultRecordForm")[0].reset()
-                // if(data=='success'){
-                //     alert('success');
-                // }
-                // else{
-                //     alert('Failed to find ID');
-                // }
